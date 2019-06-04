00451c15e1060a4852341d260e29407b045938c6
jihuiquan <jihuiquan@machinsight.com>
2019-06-04 11:31:32 +0800

    替换友盟统计key自己的

--- ziyijinfu/app/build/generated/source/dataBinding/al/debug/android/databinding/layouts/DataBindingInfo.java
+++ ziyijinfu/app/build/generated/source/dataBinding/al/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="bdec87fe-5921-4047-a5b6-ef9379e1c29b")
+@BindingBuildInfo(buildId="98dc35f4-bd5e-4027-a5bb-097f88db97eb")
 public class DataBindingInfo {}
