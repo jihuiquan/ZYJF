47d9a5ed74cb4763581c29abc9257ddc2d591050
jihuiquan <jihuiquan@machinsight.com>
2019-06-05 11:43:41 +0800

    无修改

--- ziyijinfu/app/build/generated/source/r/al/debug/com/youth/banner/R.java
+++ ziyijinfu/app/build/generated/source/r/al/debug/com/youth/banner/R.java
@@ -7,4 +7,75 @@
 package com.youth.banner;
 
 public final class R {
+    public static final class animator {
+        public static final int scale_with_alpha = 0x7f060001;
+    }
+    public static final class attr {
+        public static final int banner_default_image = 0x7f010116;
+        public static final int banner_layout = 0x7f010115;
+        public static final int delay_time = 0x7f010109;
+        public static final int image_scale_type = 0x7f010117;
+        public static final int indicator_drawable_selected = 0x7f010113;
+        public static final int indicator_drawable_unselected = 0x7f010114;
+        public static final int indicator_height = 0x7f010111;
+        public static final int indicator_margin = 0x7f010112;
+        public static final int indicator_width = 0x7f010110;
+        public static final int is_auto_play = 0x7f01010b;
+        public static final int rciv_radius = 0x7f010235;
+        public static final int scroll_time = 0x7f01010a;
+        public static final int title_background = 0x7f01010c;
+        public static final int title_height = 0x7f01010f;
+        public static final int title_textcolor = 0x7f01010d;
+        public static final int title_textsize = 0x7f01010e;
+    }
+    public static final class drawable {
+        public static final int black_background = 0x7f020095;
+        public static final int gray_radius = 0x7f0200d8;
+        public static final int long_pic = 0x7f02013b;
+        public static final int no_banner = 0x7f020166;
+        public static final int short_pic = 0x7f0201bb;
+        public static final int white_radius = 0x7f020217;
+    }
+    public static final class id {
+        public static final int bannerContainer = 0x7f0f0004;
+        public static final int bannerDefaultImage = 0x7f0f0005;
+        public static final int bannerTitle = 0x7f0f0006;
+        public static final int bannerViewPager = 0x7f0f0007;
+        public static final int center = 0x7f0f0046;
+        public static final int center_crop = 0x7f0f0061;
+        public static final int center_inside = 0x7f0f0062;
+        public static final int circleIndicator = 0x7f0f0008;
+        public static final int fit_center = 0x7f0f0063;
+        public static final int fit_end = 0x7f0f0064;
+        public static final int fit_start = 0x7f0f0065;
+        public static final int fit_xy = 0x7f0f0066;
+        public static final int indicatorInside = 0x7f0f000d;
+        public static final int matrix = 0x7f0f0067;
+        public static final int numIndicator = 0x7f0f0014;
+        public static final int numIndicatorInside = 0x7f0f0015;
+        public static final int titleView = 0x7f0f002f;
+    }
+    public static final class layout {
+        public static final int banner = 0x7f040055;
+    }
+    public static final class styleable {
+        public static final int[] Banner = { 0x7f010109, 0x7f01010a, 0x7f01010b, 0x7f01010c, 0x7f01010d, 0x7f01010e, 0x7f01010f, 0x7f010110, 0x7f010111, 0x7f010112, 0x7f010113, 0x7f010114, 0x7f010115, 0x7f010116, 0x7f010117 };
+        public static final int Banner_banner_default_image = 13;
+        public static final int Banner_banner_layout = 12;
+        public static final int Banner_delay_time = 0;
+        public static final int Banner_image_scale_type = 14;
+        public static final int Banner_indicator_drawable_selected = 10;
+        public static final int Banner_indicator_drawable_unselected = 11;
+        public static final int Banner_indicator_height = 8;
+        public static final int Banner_indicator_margin = 9;
+        public static final int Banner_indicator_width = 7;
+        public static final int Banner_is_auto_play = 2;
+        public static final int Banner_scroll_time = 1;
+        public static final int Banner_title_background = 3;
+        public static final int Banner_title_height = 6;
+        public static final int Banner_title_textcolor = 4;
+        public static final int Banner_title_textsize = 5;
+        public static final int[] RoundCornerImageView = { 0x7f010235 };
+        public static final int RoundCornerImageView_rciv_radius = 0;
+    }
 }
